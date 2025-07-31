675d32030aa1ee8a89d3a63b9aa933373148ed8a
Hasono <2105689721@qq.com>
2025-07-31 22:37:26 +0800

    feat: 添加管理员账号

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,29 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Saturn-AI-BE!");
+app.get("/", async (req, res) => {
+  const prisma = new PrismaClient();
+  const admin = await prisma.user.findUnique({
+    where: { username: "admin" },
+    select: {
+      id: true,
+      username: true,
+      nickname: true,
+      avatar: true,
+      createdAt: true,
+    },
+  });
+  if (admin) {
+    res.json({
+      message: "Admin info",
+      admin,
+    });
+  } else {
+    res.status(404).json({ message: "Admin not found" });
+  }
 });
 
 app.listen(PORT, () => {
