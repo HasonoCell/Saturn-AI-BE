5589483d6e6639131fbe81f408d7d10a00e3c8f7
Hasono <2105689721@qq.com>
2025-08-06 16:21:06 +0800

    chore: 更新依赖并调整项目结构

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { PrismaClient } from "./generated/prisma";
+import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
   const prisma = new PrismaClient();
